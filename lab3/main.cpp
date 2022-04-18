a06775f11af495b269451661f4c85f4ec314349c
Filip Constantinos Marin <filipposhyper02@gmail.com>
2022-04-18 20:00:27 +0300

    Initial Commit

---
+++ lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab3
+//
+//  Created by Filippos Marin on 18/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
