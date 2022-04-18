d9d75f210e387bc1e165307b84e875254b6814f1
Filip Constantinos Marin <filipposhyper02@gmail.com>
2022-04-18 21:03:56 +0300

    Finished sumOddArray function

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -149,10 +149,22 @@ void displayArray(const int arr[], const int size){
     cout << "\nThe total is: " << sum;
 }
 
-// Sums the odd numbers in the array and returns the result
+/**
+ * <code>sumOddArray</code> returns the value of the odd number
+ * found in the array.
+ * <BR>
+ * @param ar The array containing the values
+ * @param size The size of the array.
+ */
 int sumOddArray(const int arr[], const int size){
-//@TODO: You will need to complete this. Including makeing the appropriate comment header
-    return 0;
+    int sum=0;
+    
+    for (int i=0; i<size; i++){
+        if(arr[i]%2 != 0){
+            sum=sum+arr[i];
+        }
+    }
+    return sum;
 }
 
 // If all the values in the array are positive return true
