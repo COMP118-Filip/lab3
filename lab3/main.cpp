3c26441be0efb9390793a04e780a5bb1be6e9fbc
Filip Constantinos Marin <filipposhyper02@gmail.com>
2022-04-18 21:06:52 +0300

    Finished avgOddArray function

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -193,9 +193,23 @@ bool isAllPositive(const int arr[], const int size){
  
        return flag;
     }
-}
 
-// Finds the average of all the odd numbers in the array and stores this in the last argument
+/**
+ * <code>avgOddArray</code> calculates the average of the odd numbers in the array
+ * and store it in avgOdd integer
+ * <BR>
+ * @param arr The array containing the values
+ * @param size The size of the array.
+ * @param avgOdd Used by reference to store the average value
+ */
 void avgOddArray(const int arr[], const int size, int& avgOdd){
-    //@TODO: You will need to complete this. Including makeing the appropriate comment header
+    int sum=0;
+    
+    for (int i=0; i<size; i++){
+        if(arr[i]%2 != 0){
+            sum=sum+arr[i];
+        }
+    }
+    
+    avgOdd=sum/size;
 }
