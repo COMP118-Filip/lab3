ef4f92bd6970e1fecf320a988afde3d4565b8b2c
Filip Constantinos Marin <filipposhyper02@gmail.com>
2022-04-18 20:39:00 +0300

    changes in displayArray function

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -139,9 +139,9 @@ void multArrays(const int arrQuantity[], const int arrPrice[], int arrTotal[], c
  * @param size The size of the array.
  */
 void displayArray(const int arr[], const int size){
-    int sum;
+    int sum=0;
 
-    for (int i = 1; i < size; ++i){
+    for (int i = 0; i < size; ++i){
         cout << "\nValue at " << i << ": " << arr[i];
         sum += arr[i];
     }
